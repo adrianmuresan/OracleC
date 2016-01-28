4e5ff74d28f47947fbf807e3b5020bb4720017d6
vexo <adriann.muresan@gmail.com>
2016-01-28 02:33:29 -0500

    HardwoodSellerC.cpp update
    Worked on reading the text from file and outputting the order info

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -1,32 +1,70 @@
 //============================================================================
 // Name        : HardwoodSellerC.cpp
-// Author      : Esteban Parra
-// Version     : 1
-// Copyright   : It's free
 // Description : Main class for the Hardwood Seller
 //============================================================================
 
 #include <iostream>
 using namespace std;
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <list>
+#include <string>
 
 int main() {
-	cout
-			<< "This is an empty main method"
-			<< endl;
+	cout << "This is the order information: " << endl << endl;"
+	readInputFile("order.txt");
 	return 0;
 }
 
 /*
  * Method to read the input file
  */
-void readInputFile(string inputFilePath) {
+void readInputFile(string inputFilePath) 
+{
+	string customerName;
+	string address;
+	string date;
+	string woodtype;
+	string bf;
+	
+	double total = 0;
+	double max = 0;
 
-}
-
-/*
- * Method to compute the deliveryTime
- */
-double deliveryTime() {
-	double deliveryETA = 0.0;
-	return deliveryETA;
-}
+	std::list<WoodItem> mylist;
+	ifstream inputFile;
+	inputFile.open(inputFilePath);
+    
+	getline(inputFile, customerName, ';');
+	getline(inputFile, address, ';');
+	getline(inputFile, date, ';');
+	
+	while(getline(inputFile, woodtype, ';'))
+	{
+	getline(inputFile, bf, ';');
+	int x = stoi(bf);
+	WoodItem temp(woodtype, x, 0);
+	mylist.push_back(temp);
+	total = total + temp.getPrice();
+	if (max < temp.getTime())
+	{
+	max = temp.getTime();
+	}
+	// output after reading the order text
+	
+	cout << "Customer : " << customerName << endl << endl;
+	
+	cout << "Address : " << address << endl << endl;
+	
+	cout << Wood\tPrice << endl << endl; 
+	
+	for (std::list<WoodItem>::iterator it = mylist.begin(); it != mylist.end(); ++it)
+	{
+	cout << (*it).woodtype << "\t\t" << (*it).price << endl; 	
+	}
+	cout << "Estimated Time : " << max << endl << endl; 
+	
+	cout << "Total : " << "$" << total << endl << endl; 
+	
+	cin,get() // pause the console
+	}
