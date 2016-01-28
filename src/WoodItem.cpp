b73cd842e8312a010058b099b8965a484e358d21
vexo <adriann.muresan@gmail.com>
2016-01-28 04:38:00 -0500

    WoodItem.cpp addition
    Added shipping time calculation

--- src/WoodItem.cpp
+++ src/WoodItem.cpp
@@ -1,3 +1,4 @@
+
 #include "WoodItem.h"
 #include<cstring>
 using namespace std;
@@ -49,3 +50,76 @@ WoodItem::WoodItem(string woodType,
 	}
 
 }
+
+// Shipping time calculation
+
+
+double WoodItem::getTime() 
+{
+	double shippingTime = 0;
+	if (baseDeliveryTime >= 1 && baseDeliveryTime <= 100) 
+	{
+		shippingTime = 1;
+	} 
+	else if (baseDeliveryTime >= 101 && baseDeliveryTime <= 200) 
+	{
+		shippingTime = 2;
+	} 
+	else if (baseDeliveryTime >= 201 && baseDeliveryTime <= 300) 
+	{
+		shippingTime = 3;
+	} 
+	else if (baseDeliveryTime >= 301 && baseDeliveryTime <= 400) 
+	{
+		shippingTime = 4;
+	} 
+	else if (baseDeliveryTime >= 401 && baseDeliveryTime <= 500) 
+	{
+		shippingTime = 5;
+	} 
+	else if (baseDeliveryTime >= 501 && baseDeliveryTime <= 1000) 
+	{
+		shippingTime = 5.5;
+	}
+	if (strcmp(typeWood.c_str(), "Cherry") == 0) 
+	{
+		shippingTime *= 2.5;
+	}
+
+	else if (strcmp(typeWood.c_str(), "Curly Maple") == 0) 
+	{
+		shippingTime *= 1.5;
+
+	} 
+	else if (strcmp(typeWood.c_str(), "Genuine Mahogany") == 0) 
+	{
+		shippingTime *= 3;
+
+	} 
+	else if (strcmp(typeWood.c_str(), "Wenge") == 0) 
+	{
+		shippingTime *= 5;
+
+	} 
+	else if (strcmp(typeWood.c_str(), "White Oak") == 0) 
+	{
+		shippingTime *= 2.3;
+
+	} 
+	else if (strcmp(typeWood.c_str(), "Sawdust") == 0) 
+	{
+		shippingTime *= 1;
+
+	}
+	return shippingTime;
+}
+double WoodItem::getPrice() 
+{
+
+	return price;
+}
+string WoodItem::getType() 
+{
+	return typeWood;
+}
+
